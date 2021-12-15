fc262ed6d91d4cf24073af97bba54e115ad0696d
moshe <moshedoppelt@gmail.com>
2021-12-15 16:17:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,7 +26,12 @@ cp .tmux/.tmux.conf.local .
 
 
 sudo apt install python3-pip -y
+
+echo '[*] Install vim'
+
 sudo apt install vim -y
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh`
 
 if [ $# -eq 0 ]
 then
