5da1de6b2383b4ef56196a2e2d7d804682eace42
moshe <moshedoppelt@gmail.com>
2021-12-29 11:06:16 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,6 +33,11 @@ sudo apt install vim -y
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh`
 
+echo '[*] Install ctf-tools'
+
+git clone https://github.com/zardus/ctf-tools.git
+./ctf-tools/bin/manage-tools setup
+
 if [ $# -eq 0 ]
 then
   echo "Usage: ..."
