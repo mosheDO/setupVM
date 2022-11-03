31b64790f0f6d11368ddbf1701b1527367d7ff53
moshe <moshedoppelt@gmail.com>
2022-11-04 00:33:01 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,74 +3,73 @@
 cd ~
 
 sudo apt-get update
+sudo apt -y upgrade
 sudo apt-get install xclip  -y
 
-if [ $# -eq 0 ]
-then
-    sudo apt-get install build-essential linux-headers-$(uname -r) -y
-    sudo apt-get install virtualbox-guest-additions-iso -y
 
-    sudo apt install build-essential libtool  tree g++ gcc texinfo curl wget automake autoconf python3 python3-pip python3-dev libssl-dev libffi-dev subversion unzip libedit-dev nasm virtualenvwrapper lsb-release -y
-    sudo apt-get install ruby-full -y
-    sudo gem install one_gadget
-    sudo gem install seccomp-tools
-    sudo apt install binwalk -y 
-    sudo apt install elfutils -y 
-    sudo apt install patchelf -y 
-     
-    
-    echo '[*] Install git curl'
-    sudo apt install ipython3 -y
-    sudo apt install curl -y 
-    sudo apt install git -y 
-    pip3 install chepy
+sudo apt-get install build-essential linux-headers-$(uname -r) -y
+sudo apt-get install virtualbox-guest-additions-iso -y
 
-    echo '[*] Install tmux'
+sudo apt install build-essential libtool  tree g++ gcc texinfo curl wget automake autoconf python3 python3-pip python3-dev libssl-dev libffi-dev subversion unzip libedit-dev nasm virtualenvwrapper lsb-release -y
+sudo apt-get install ruby-full -y
+sudo gem install one_gadget
+sudo gem install seccomp-tools
+sudo apt install binwalk -y 
+sudo apt install elfutils -y 
+sudo apt install patchelf -y 
 
-    sudo apt install tmux -y
 
-    echo '[*] Install custom conf tmux'
+echo '[*] Install git curl'
+sudo apt install ipython3 -y
+sudo apt install curl -y 
+sudo apt install git -y 
+pip3 install chepy
 
-    git clone https://github.com/gpakosz/.tmux.git
-    ln -s -f .tmux/.tmux.conf
-    cp .tmux/.tmux.conf.local .
-    cp ./setupVM/conf/.tmux.conf.local .
+echo '[*] Install tmux'
 
-    echo '[*] Install pwntools'
+sudo apt install tmux -y
 
-    python3 -m pip install --upgrade pip
-    python3 -m pip install --upgrade pwntools
+echo '[*] Install custom conf tmux'
 
-    wget https://github.com/io12/pwninit/releases/download/3.2.0/pwninit -P ~/.local/bin/
-    chmod +x ~/.local/bin/pwninit
-    
-    echo '[*] Install vim'
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+cp ./setupVM/conf/.tmux.conf.local .
 
-    sudo apt install vim -y
-    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-    sh ~/.vim_runtime/install_awesome_vimrc.sh
+echo '[*] Install pwntools'
 
-    cp ./setupVM/conf/.vimrc ~/.vim_runtime/my_configs.vim
-    echo '[*] Install ctf-tools'
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pwntools
 
-    git clone https://github.com/zardus/ctf-tools.git
-    ./ctf-tools/bin/manage-tools setup
-     
-    git clone https://github.com/pwndbg/pwndbg
-    cd pwndbg
-    ./setup.sh
-    
-    if [ -d ~/.oh-my-zsh ]; then
-        echo "oh-my-zsh is installed"
-    else
-        echo '[*] Install zsh & oh my zsh'
-        sudo apt install zsh -y 
-        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-        echo 'export PATH="~/.local/bin/:$PATH"' >> .zshrc
-        chsh -s $(which zsh)
-    fi
-    source .zshrc
+wget https://github.com/io12/pwninit/releases/download/3.2.0/pwninit -P ~/.local/bin/
+chmod +x ~/.local/bin/pwninit
+
+echo '[*] Install vim'
+
+sudo apt install vim -y
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+cp ./setupVM/conf/.vimrc ~/.vim_runtime/my_configs.vim
+echo '[*] Install ctf-tools'
+
+git clone https://github.com/zardus/ctf-tools.git
+./ctf-tools/bin/manage-tools setup
+
+git clone https://github.com/pwndbg/pwndbg
+cd pwndbg
+./setup.sh
+
+if [ -d ~/.oh-my-zsh ]; then
+    echo "oh-my-zsh is installed"
+else
+    echo '[*] Install zsh & oh my zsh'
+    sudo apt install zsh -y 
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    echo 'export PATH="~/.local/bin/:$PATH"' >> .zshrc
+    chsh -s $(which zsh)
 fi
+source .zshrc
 
 # IS_PWN=flase 
 # for i in "$@" ; do
